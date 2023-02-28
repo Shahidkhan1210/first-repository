a4787c469bbc75dcad30a7c0028be132d5809d81
shahid <shahidakbar1210@gmail.com>
2023-02-28 13:05:06 -0800

    first commit

---
+++ app.js
@@ -0,0 +1,183 @@
+// var a = " shahid "
+// var d = "  student"
+// // var temp = `${a}is a nice person \n  ${d} of kms `
+// // console.log(a+ d)
+// var a = "sameer"
+
+// let b = "asad"
+// {
+// let b = "khan"} 
+// console.log(b)
+
+
+// let age = 12
+// if(age >=18){
+//     document.write( "you are eligible to drive")
+// }
+// else{
+//     document.write
+//     ( "you are not eligible to drive"  )
+// }
+
+// let age = 8
+
+// if(age >= 5){
+//     document.write( " he can go to school" )
+// }
+// else { 
+//     document.write("she can't" )
+// }
+
+// let age =  11; 
+
+// if( age >18 ){
+//     document.write ("you can drive")
+// }
+// else if ( age == 18){
+//   document.write  ("you should apply license")
+// }
+// else{ document.write  ("you cannot")}
+
+// var dayName = "sunday"
+// switch (dayName) {
+//     case "monday":
+//         console.log("it is monday")
+//         break;
+
+//         case "friday":
+//         console.log("it is friday")
+//         break;
+
+
+//         case "saturday":
+//         console.log("it is saturday")
+//         break;
+
+
+//     default:
+//         console.log("leave day")
+//         break;
+// // } 
+// var a = 5
+// console.log(a+4)
+
+
+
+// var name = "sameer" 
+// var lastName  = "yousafzai"
+
+// document.write( "<h1>" + name  + "     " + " " + lastName + "</h1>" )
+
+
+// var name  =  prompt("enter your name", "asim")
+// document.write(name)
+
+// var phoneNumber = prompt("dial your phone number" , 452  )
+
+// document.write(prompt("hello"))
+
+
+// var num1 = +prompt("enter num 1 ")
+// var opt = prompt("enter operator")
+// var num2 = +prompt("enter num 2 ")
+
+// if(opt == "+"){
+//     alert ( num1 + num2 )
+// }
+// if(opt =="-"){
+//   alert(num1 - num2)
+// }
+// if(opt =="*"){ 
+//     alert(num1 * num2)
+// }
+
+
+// var food = prompt(" enter the food")
+// var extraFood = prompt("enter extra")
+
+// if( food === "chicken" ){
+//     alert( "lai awo" )
+// }
+// else if (food === "biryani"){
+//     if( extrafood === "raita"   ){
+//         alert (" 10 ka lai awo ")
+//     } 
+//     else{ alert (" ajao yar")}
+//     alert("aik plata lai awo")
+// }
+// else if ( food === "daal" )
+//  alert("50 ke taili mai lai awo")
+// }
+// else{
+//      alert("paise wapis lai awo") 
+// }
+
+// var lang1 = prompt("enter your first language")
+// var lang2 = prompt("enter your second language")
+// if( lang1 === "english" && lang2 === "urdu"){
+// alert( "you are hired!")
+// }
+// else {
+//     alert("you are not hired") 
+// } 
+
+// function name(params) {
+    
+// // }
+// function myintro(){
+
+//     alert(
+//         "i am sameer "
+//     )
+//     document.write(" i am learning javascript")
+
+// }
+// myintro();
+
+
+
+function addition(a,b){
+    alert(a*b)
+}
+
+
+addition(2,3)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
